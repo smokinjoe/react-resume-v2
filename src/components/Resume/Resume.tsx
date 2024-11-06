ac1f03f8f63960722485c68ddfe462a0c0df0bf7
Joe <joe.ekiert@gmail.com>
2024-11-05 23:37:12 -0500

    building out the resume layout (rough draft)

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,3 +1,5 @@
+import { Grid, Title, Text } from "@mantine/core";
+
 import { useGetResume } from "../../hooks/useGetResume";
 
 const Resume = () => {
@@ -11,9 +13,42 @@ const Resume = () => {
     return <div>Error: {error.message}</div>;
   }
 
+  if (!data) {
+    return <div>No data</div>;
+  }
+
   return (
     <div>
-      <code>{JSON.stringify(data)}</code>
+      <Grid>
+        <Grid.Col span={8}>
+          <Title order={1}>{data.name}</Title>
+          <Text>
+            An engineer looking for challenges and an opportunity to build great
+            products.
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Text>{data.address.street}</Text>
+          <Text>
+            {data.address.city}, {data.address.state} {data.address.zip}
+          </Text>
+          <Text fw={700}>{data.phone}</Text>
+          <Text fw={700}>{data.email}</Text>
+        </Grid.Col>
+        <Grid.Col span={8}>
+          <Title order={2}>Experience</Title>
+          {data.employmentHistory.map((exp) => (
+            <div key={exp.company}>
+              <Text>{exp.company}</Text>
+              <Text>{exp.title}</Text>
+              <Text>
+                {exp.dateStart} - {exp.dateEnd}
+              </Text>
+              <Text>{JSON.stringify(exp.experience)}</Text>
+            </div>
+          ))}
+        </Grid.Col>
+      </Grid>
     </div>
   );
 };
