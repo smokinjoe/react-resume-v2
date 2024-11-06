163b3cebc31db7df888c4b591b438fe7cc0dfe06
Joe <joe.ekiert@gmail.com>
2024-11-05 23:36:52 -0500

    left align text

--- src/theme.ts
+++ src/theme.ts
@@ -2,4 +2,20 @@ import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
   /** Put your mantine theme override here */
+  components: {
+    Text: {
+      styles: {
+        root: {
+          textAlign: "left",
+        },
+      },
+    },
+    Title: {
+      styles: {
+        root: {
+          textAlign: "left",
+        },
+      },
+    },
+  },
 });
